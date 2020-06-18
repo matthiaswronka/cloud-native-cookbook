347c858a8b264f5398af4aa2c8c704d82a37b1aa
Matthias Wronka <wronka@finpair.de>
2020-06-18 16:51:25 +0200

    add api-test

--- modules/sample-rest-spring-boot/src/main/java/de/nordlb/example/person/PersonController.java
+++ modules/sample-rest-spring-boot/src/main/java/de/nordlb/example/person/PersonController.java
@@ -5,7 +5,11 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,15 @@ public class PersonController {
           repository.findAll().spliterator(), false)
           .collect(Collectors.toList());
     }
+  }
+
+  @PostMapping("/persons")
+  public void postPerson(@RequestBody Person person) {
+    repository.save(person);
+  }
+
+  @DeleteMapping("/persons/{id}")
+  public void deletePerson(@PathVariable Long id) {
+    repository.deleteById(id);
   }
 }
